222d0e23721394d3b31eba1c94f79658884b90a1
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-12-14 12:08:03 -0600

    prints everything on boards

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import authData from '../../helpers/data/authData';
 import boardData from '../../helpers/data/boardData';
+import Board from '../Board/Board';
 
 
 class BoardsContainer extends React.Component {
@@ -18,7 +19,11 @@ class BoardsContainer extends React.Component {
   }
 
   render() {
-    return (<div>{this.state.boards.map((board) => <h6>{board.name}</h6>)}</div>);
+    return (
+      <div>
+        {this.state.boards.map((board) => (<Board key={board.id} board={board}/>))}
+      </div>
+    );
   }
 }
 
